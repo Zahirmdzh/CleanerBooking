ea1053d1ba83f0b58607563b72fc5ab80cd5c1db
17042725 <zahir.mdzh@gmail.com>
2019-05-15 14:56:25 +0800

    ii

---
+++ app/src/main/java/sg/edu/rp/c300/cleanerbooking/ContactActivity.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.c300.cleanerbooking;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        Toolbar myTB = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myTB);
+        ActionBar AB = getSupportActionBar();
+        AB.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
